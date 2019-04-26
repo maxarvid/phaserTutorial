435532acea4367d6e490d0b06847be7ab9e88fb3
Max <max.arvid.anderson@gmail.com>
2019-04-26 11:58:39 +0200

    part 5

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ function create ()
   this.add.image(400, 300, 'sky');
   // this.add.image(400, 300, 'star');
 
+  // Add the static platforms
   platforms = this.physics.add.staticGroup();
 
   platforms.create(400, 548, 'ground').setScale(2).refreshBody();
@@ -44,6 +45,31 @@ function create ()
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground')
 
+  // Add the player
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
 
 }
 
