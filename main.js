1a41a27b81b708e3f0b113fcbdd082bf6a4d9205
Max <max.arvid.anderson@gmail.com>
2019-04-26 13:06:43 +0200

    part 10

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ var cursors;
 var stars;
 var score = 0;
 var scoreText;
+var bombs;
 
 var game = new Phaser.Game(config);
 
@@ -102,6 +103,13 @@ function create ()
   // keep track of the score
   scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '24px', fill: '#000' });
 
+  // adding the baddies
+  bombs = this.physics.add.group();
+
+  this.physics.add.collider(bombs, platforms);
+
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
+
 }
 
 function update ()
@@ -138,4 +146,31 @@ function collectStar (player, star)
 
   score += 10;
   scoreText.setText('score: ' + score);
+
+  if (stars.countActive(true) === 0)
+  {
+    stars.children.iterate(function(child) {
+
+      child.enableBody(true, child.x, 0, true, true);
+
+    });
+
+    var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+    var bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  }
+}
+
+function hitBomb (player, bomb)
+{
+  this.physics.pause();
+
+  player.setTint(0xff000);
+
+  player.anims.play('turn');
+
+  gameOver = true;
 }
